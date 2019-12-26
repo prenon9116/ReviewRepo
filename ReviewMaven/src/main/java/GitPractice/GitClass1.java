684e8fe380c7eca746ebd6ee0c9d574b896e4529
prenon9116 <prenon9116@gmail.com>
2019-12-26 09:36:14 -0500

    Modify project v1.2

--- ReviewMaven/src/main/java/GitPractice/GitClass1.java
+++ ReviewMaven/src/main/java/GitPractice/GitClass1.java
@@ -2,7 +2,14 @@ package GitPractice;
 
 public class GitClass1 {
 	public static void main(String[] args) {
-		
+		/*
+		 * How do you work using Git
+		 * step1
+		 * step2 
+		 * step 3
+		 * step 4
+		 * 
+		 */
 	}
 
 }
